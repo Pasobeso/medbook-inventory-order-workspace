0a81ab1535d9e883863c7b20477af3855e5208d0
Ittiwat Chuchoet <ciaabcdefg@gmail.com>
2025-10-14 16:19:11 +0700

    added get inventory by id

--- medbook-inventoryservice/src/routes/inventory.rs
+++ medbook-inventoryservice/src/routes/inventory.rs
@@ -1,10 +1,23 @@
-use crate::{app_error::AppError, app_state::AppState, models::ProductInventoryEntity};
+use crate::{
+    app_error::AppError, app_state::AppState, models::ProductInventoryEntity,
+    schema_custom::product_inventory_view,
+};
 use anyhow::{Context, Result};
-use axum::{Json, Router, extract::State, response::IntoResponse, routing};
+use axum::{
+    Json, Router,
+    extract::{Path, State},
+    response::IntoResponse,
+    routing,
+};
+
+use diesel::QueryDsl;
 use diesel_async::RunQueryDsl;
+use tracing::error;
 
 pub fn routes() -> Router<AppState> {
-    Router::new().route("/", routing::get(get_inventory))
+    Router::new()
+        .route("/", routing::get(get_inventory))
+        .route("/{id}", routing::get(get_inventory_by_product_id))
 }
 
 async fn get_inventory(State(state): State<AppState>) -> Result<impl IntoResponse, AppError> {
@@ -14,11 +27,37 @@ async fn get_inventory(State(state): State<AppState>) -> Result<impl IntoRespons
         .await
         .context("Failed to obtain a DB connection pool")?;
 
-    let inventory: Vec<ProductInventoryEntity> =
-        diesel::sql_query("select * from product_inventory_view")
-            .get_results(conn)
-            .await
-            .context("Failed to fethc")?;
+    let inventory: Vec<ProductInventoryEntity> = product_inventory_view::table
+        .order_by(product_inventory_view::product_id)
+        .get_results(conn)
+        .await
+        .context("Failed to fetch inventory")?;
 
     Ok(Json(inventory))
+}
+
+async fn get_inventory_by_product_id(
+    Path(id): Path<i32>,
+    State(state): State<AppState>,
+) -> Result<impl IntoResponse, AppError> {
+    let conn = &mut state
+        .db_pool
+        .get()
+        .await
+        .context("Failed to obtain a DB connection pool")?;
+
+    match product_inventory_view::table
+        .find(id)
+        .get_result(conn)
+        .await
+    {
+        Ok(inventory) => {
+            let inventory: ProductInventoryEntity = inventory;
+            Ok(Json(inventory))
+        }
+        Err(err) => {
+            error!("Error in get_inventory_by_product_id: {}", err);
+            Err(AppError::ProductNotFound(id))
+        }
+    }
 }
